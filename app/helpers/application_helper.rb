10d79646accaf2ef8ba1fabab65e4eaffd823ba9
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-12-18 15:28:23 +0300

    Add flash messages styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    alert_class = {
+      "success" => "alert-success",
+      "error" => "alert-danger",
+      "notice" => "alert-info",
+      "alert" => "alert-danger",
+      "warn" => "alert-warning"
+    }
+    alert_class[level]
+  end
 end
